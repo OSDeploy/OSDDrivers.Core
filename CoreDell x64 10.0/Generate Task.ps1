7d9c191845d1707f28ad810099b8e24ec90a5b0c
David Segura <david@segura.org>
2019-07-28 16:41:48 -0500

    19.7.28

--- CoreDell x64 10.0/Generate Task.ps1
+++
@@ -1,8 +0,0 @@
-﻿Import-Module OSDDrivers -Force
-$MakeMatch = 'Dell'
-$OSVersionMatch = '10.0'
-$OSArchMatch = 'x64'
-
-Get-ChildItem $PSScriptRoot *.cab | foreach {
-    New-OSDDriverTask -OSDDriverFile "$($_.FullName)" -MakeMatch $MakeMatch -OSVersionMatch $OSVersionMatch -OSArchMatch $OSArchMatch
-}
