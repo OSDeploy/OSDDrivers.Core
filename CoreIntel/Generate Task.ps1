4ddea3680151f692c2621021917a8150f66b78c3
David Segura <david@segura.org>
2019-07-28 16:29:34 -0500

    19.7.28

---
+++ CoreIntel/Generate Task.ps1
@@ -0,0 +1,20 @@
+﻿Import-Module OSDDrivers -Force
+$OSVersionMatch = '10.0'
+
+
+
+Get-ChildItem $PSScriptRoot *.cab | foreach {
+    $OSArchMatch = 'x64'
+    $OSInstallationType = $null
+    if ($_.Name -match 'x86') {$OSArchMatch = 'x86'}
+    if ($_.Name -match 'Client') {$OSInstallationType = 'Client'}
+    
+    
+
+    if ($_.Name -match 'IntelBluetooth') {New-OSDDriverTask -OSDDriverFile "$($_.FullName)" -OSArchMatch $OSArchMatch}
+    if ($_.Name -match 'IntelChipset 10') {New-OSDDriverTask -OSDDriverFile "$($_.FullName)" -OSArchMatch $OSArchMatch -OSInstallationType $OSInstallationType}
+    #if ($_.Name -match 'IntelChipset ME') {New-OSDDriverTask -OSDDriverFile "$($_.FullName)" -OSArchMatch $OSArchMatch}
+
+
+    
+}
